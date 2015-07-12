e689a74b88abece70a2d6ae8e89824bfea9f1c3e
shile <1019606614@qq.com>
2015-07-12 20:01:38 +0800

    Initial Commit

---
+++ WYMenuAnimate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WYMenuAnimate
+//
+//  Created by Kevin on 15/7/12.
+//  Copyright (c) 2015年 石乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WYMenuAnimate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WYMenuAnimate
+//
+//  Created by Kevin on 15/7/12.
+//  Copyright (c) 2015年 石乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
